c3180b62c99f0076ef46ae976ec1bb49907e5a35
Hamou Ait Abderrahim <gh_aitabderrahim@esi.dz>
2024-07-04 15:01:38 -0400

    Update print_server.py

--- print_server.py
+++ print_server.py
@@ -2,12 +2,38 @@ from flask import Flask, request, jsonify
 import subprocess
 import os
 from flask_cors import CORS
-CORS(app)
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some-super-secret-key'
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow specific origin
+socketio = SocketIO(app, cors_allowed_origins="*")  # Allow specific origin
 
 printer_name = "EPSON TM-T88V Receipt"
 file_path = r"c:\users\kiosk-1\desktop\printfile.txt"
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return 'data'
+
+@socketio.on('sandwich')
+def handle_sandwich_event(data):
+    print('Sandwich Event: ' + data)
+    emit('sandwich', data, broadcast=True)
+
+@socketio.on('deli')
+def handle_deli_event(data):
+    print('Deli Event: ' + data)
+    emit('deli', data, broadcast=True)
+
+@socketio.on('disconnect')
+def disconnect(data):
+    print('Disconnected')
+
+@socketio.on_error_default  # Default handler for all errors
+def error_handler(e):
+    print('An error has occurred:', e)
+
 
 @app.route('/print', methods=['POST'])
 def print_file():
@@ -15,13 +41,12 @@ def print_file():
         data = request.get_json()
         if not data:
             print("No JSON data received")
-            return "No JSON data received", 400
-
-        content = data.get('content', '')
+            return jsonify({"code": 400, "status": "error", "msg": "No JSON data received"}), 400
         
+        content = data.get('content', '')
         if not content:
             print("Received content is empty")
-            return "Received content is empty", 400
+            return jsonify({"code": 400, "status": "error", "msg": "Received content is empty"}), 400
         else:
             print(f"Received content: '{content}'")
         
@@ -43,18 +68,22 @@ def print_file():
             print(f"File content: '{file_content}'")
         else:
             print(f"File {file_path} does not exist.")
-            return f"File {file_path} does not exist.", 500
+            return jsonify({"code": 500, "status": "error", "msg": f"File {file_path} does not exist."}), 500
         
-        # Print the file using a PowerShell command to ensure it uses the correct printera
+        # Print the file using a PowerShell command to ensure it uses the correct printer
         print(f"Attempting to print the file {file_path} using PowerShell")
         powershell_command = f'Start-Process -FilePath notepad.exe -ArgumentList "/p {file_path}" -NoNewWindow -Wait'
         result = subprocess.run(['powershell', '-Command', powershell_command], shell=True)
         print(f"Subprocess finished with return code {result.returncode}")
         
-        return 'File printed successfully', 200
+        if result.returncode == 0:
+            return jsonify({"code": 200, "status": "success", "msg": "File printed successfully"}), 200
+        else:
+            return jsonify({"code": 500, "status": "error", "msg": "Failed to print file"}), 500
+
     except Exception as e:
         print(f"An error occurred: {e}")
-        return f"An error occurred: {e}", 500
+        return jsonify({"code": 500, "status": "error", "msg": f"An error occurred: {e}"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True)
